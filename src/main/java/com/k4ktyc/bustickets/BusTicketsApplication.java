53f4c4459dccf4148571670af04d7102b41d11a0
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2021-04-04 18:26:19 +0300

    Initial commit

---
+++ src/main/java/com/k4ktyc/bustickets/BusTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.k4ktyc.bustickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTicketsApplication.class, args);
+    }
+
+}
