9741c474d14289a57953ec789052c46adbc58515
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2021-04-05 13:07:48 +0300

    Create User and userDto classes

--- src/main/java/com/k4ktyc/bustickets/model/User.java
+++ src/main/java/com/k4ktyc/bustickets/model/User.java
@@ -1,4 +1,29 @@
 package com.k4ktyc.bustickets.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
 public class User {
+
+    @Id @GeneratedValue
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    public User() {}
+
+    public User(UserDto userDto) {
+        this.username = userDto.getUsername();
+        this.password = userDto.getPassword();
+    }
 }
---
+++ src/main/java/com/k4ktyc/bustickets/model/UserDto.java
@@ -0,0 +1,17 @@
+package com.k4ktyc.bustickets.model;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Getter
+public class UserDto {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    @Pattern(regexp = "^(?=\\S+$).{5,20}$")
+    private String password;
+}
