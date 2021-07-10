f2441a77cb37a949e10510e19c75ff7c469e1d7a
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2021-07-10 14:33:45 +0300

    Adaptive design for orders page

--- src/main/resources/static/js/Orders.js
+++ src/main/resources/static/js/Orders.js
@@ -43,22 +43,16 @@ function fillPageWithPassengers(data) {
         let orders = passengerOrders[i]
 
         let pageTemplate = `
-        <div class="row order-list-content align-items-center px-5 justify-content-between" id="orders-${orders.passengerId}">
-            <div class="col-md-4 order-passenger">
-                <p class="order-passenger-text">${orders.name} ${orders.lastname}</p>
-            </div>
-            <div class="col-md-4 order-orders-amount text-center">
-                <p class="order-orders-amount-text">${orders.amount}</p>
-            </div>
+        <div class="order-list-content passengers" id="orders-${orders.passengerId}">
+            <div class="col-passenger">${orders.name} ${orders.lastname}</div>
+            <div class="col-amount"><span>Заказов: </span>${orders.amount}</div>
         </div>
-        
-        <div class="row my-1"></div>
         `
 
         $('#order-list-container').append(pageTemplate)
 
         $(`#orders-${orders.passengerId}`).on('click', () => {
-            let passenger = $(`#orders-${orders.passengerId}`).children('.order-passenger').children('.order-passenger-text').text().split(' ')
+            let passenger = $(`#orders-${orders.passengerId}`).children('.col-passenger').text().split(' ')
             let name = passenger[0]
             let lastname = passenger[1]
 
@@ -72,35 +66,43 @@ function fillPageWithOrders() {
 
     let orders = orderList
 
-    $('#order-list-container').append(`
-        <div class="row title p-5">${orders[0].passenger.name} ${orders[0].passenger.lastname}</div>
-    `)
+    $('.order-list-title').text(`${orders[0].passenger.name} ${orders[0].passenger.lastname}`)
 
     for (let i = 0; i < orders.length; i++) {
         let order = orders[i]
 
-        let startTimeDate = moment(order.trip.datetime).locale('ru')
+        let stations = order.trip.routeDto.routeStations
+        let timeGap = 0
+        for (const s of stations) {
+            if (s.stationName !== order.sstart.stationName) {
+                timeGap += s.timeGap
+            } else {
+                break;
+            }
+        }
+
+        let startTimeDate = moment(order.trip.datetime).add(timeGap, 'minutes').locale('ru')
 
         let pageTemplate = `
-        <div class="row order-list-content align-items-center px-5 justify-content-between" id="orders-${order.id}">
-            <div class="col-md-4 order-info">
+        <div class="order-list-content" id="orders-${order.id}">
+            <div class="col-content-route">
                 <p class="order-route">${order.sstart.stationName} — ${order.sfinish.stationName}</p>
             </div>
-            <div class="col-md-2 order-datetime">
-                <p class="order-time">${startTimeDate.format('HH:mm')}</p>
-                <p class="order-date">${startTimeDate.format('DD.MM.YYYY')}</p>
+            <div class="col-content-datetime-bus">
+                <div class="col-content-datetime">
+                    <p class="order-time">${startTimeDate.format('HH:mm')}</p>
+                    <p class="order-date">${startTimeDate.format('DD.MM.YYYY')}</p>
+                </div>
+                <div class="col-content-bus">
+                    <p class="order-bus-number">Автобус № ${order.trip.busDto.number}</p>
+                    <p class="order-bus-seat">Место № ${order.seat}</p>
+                </div>
             </div>
-            <div class="col-md-4 order-bus">
-                <p class="order-bus-number">Автобус № ${order.trip.busDto.number}</p>
-                <p class="order-bus-seat">Место № ${order.seat}</p>
-            </div>
-            <div class="col-md-2 order-addinfo">
+            <div class="col-content-info">
                 <p class="order-price">${order.price / 100} BYN</p>
-                <p class="order-status">${order.status}</p>
+                <p class="order-status">Статус: ${order.status}</p>
             </div>
         </div>
-        
-        <div class="row my-1"></div>
         `
 
         $('#order-list-container').append(pageTemplate)
