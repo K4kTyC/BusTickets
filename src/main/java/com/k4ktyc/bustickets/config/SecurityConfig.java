3f3399e6e732bd34612a02f422df8d4c08f366c0
K4kTyC <47088033+k4ktyc@users.noreply.github.com>
2022-03-19 00:52:18 +0300

    add docker compose file, update dependencies version

--- src/main/java/com/k4ktyc/bustickets/config/SecurityConfig.java
+++ src/main/java/com/k4ktyc/bustickets/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.k4ktyc.bustickets.config;
 import com.k4ktyc.bustickets.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,26 +14,34 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
 
-    private UserService userService;
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
+    @Bean
+    public static PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    @EnableWebSecurity
+    public static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider());
-    }
+        private UserService userService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        @Autowired
+        public void setUserService(UserService userService) {
+            this.userService = userService;
+        }
+
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) {
+            auth.authenticationProvider(authProvider());
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
                     .antMatchers("/css/**", "/js/**", "/img/**").permitAll()
                     .antMatchers("/", "/home.html").permitAll()
                     .antMatchers(HttpMethod.GET, "/trips/**", "/api/trips/**").permitAll()
@@ -42,39 +51,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/admin/**", "/api/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated()
                     .and()
-                .formLogin()
+                    .formLogin()
                     .loginPage("/?needLogin")
                     .loginProcessingUrl("/login")
                     .successHandler(successHandler())
                     .failureUrl("/?error")
                     .and()
-                .rememberMe()
+                    .rememberMe()
                     .key("thumbup")
                     .userDetailsService(userService)
                     .and()
-                .logout()
+                    .logout()
                     .deleteCookies("JSESSIONID")
                     .logoutSuccessUrl("/")
                     .and()
-                .csrf().disable();
-    }
+                    .csrf().disable();
+        }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public DaoAuthenticationProvider authProvider() {
+            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+            authProvider.setUserDetailsService(userService);
+            authProvider.setPasswordEncoder(getPasswordEncoder());
+            return authProvider;
+        }
 
-    @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService);
-        authProvider.setPasswordEncoder(getPasswordEncoder());
-        return authProvider;
+        @Bean
+        public AuthenticationSuccessHandler successHandler() {
+            return new CustomLoginSuccessHandler("/");
+        }
     }
-
-    @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return new CustomLoginSuccessHandler("/");
-    }
-
 }
