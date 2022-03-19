e7d4194acb8505838a78109176addbc1a70c1869
K4kTyC <47088033+k4ktyc@users.noreply.github.com>
2022-03-19 20:29:57 +0300

    bus model page fix

--- src/main/resources/static/js/AdminBusModels.js
+++ src/main/resources/static/js/AdminBusModels.js
@@ -1,18 +1,35 @@
-let pageNum = 0
-let lastPage = 0
+const $busModelList = $('#model-list-elements');
+
+let pagination = new Pagination(async () => {
+	await updateContent();
+
+	const offsetPosition = $busModelList.offset().top - $('#navbar-main').outerHeight();
+	window.scrollTo({
+		top: offsetPosition,
+		behavior: "smooth"
+	});
+});
+
+const UISelectContainer = new Map(); // key - description, value - CustomSelect instance
+const defaultBusModelPlaceholderAmount = 16;
+
 let busClassList
 let busModelList
+let busModelPlaceholderAmount = 0;
 
 $(() => {
-	getBusClassList().then(fillSelectWithClasses)
-	getBusModels().then(fillPageWithModels)
+	getBusClassList().then(() => {
+		createUISelectObject();
+		fillUISelectWithClasses();
+	});
+	pagination.refresh();
 })
 
 $('#model-submit').on('click', () => {
 	let id
-
+	const $busClass = UISelectContainer.get('bus-class').$input;
 	for (const c of busClassList) {
-		if (c.name === selectParts[0].$input.val()) {
+		if (c.name === $busClass.val()) {
 			id = c.id
 			break
 		}
@@ -43,97 +60,88 @@ async function getBusClassList() {
 	busClassList = await response.json()
 }
 
+async function updateContent() {
+	let p1 = startLoadingAnimation();
+	let p2 = getBusModels();
+	await p1;
+	await p2;
+
+	fillPageWithModels();
+
+	setTimeout(() => {
+		$busModelList.addClass('updated');
+	}, 1);
+	setTimeout(() => {
+		$busModelList.removeClass('updating');
+		$busModelList.removeClass('updated');
+	}, 300);
+}
+
+async function startLoadingAnimation() {
+	$busModelList.addClass('updating');
+
+	// wait for opacity transition
+	await new Promise(r => setTimeout(r, 250));
+
+	$busModelList.find('.models-list-content').remove();
+
+	if (busModelPlaceholderAmount < defaultBusModelPlaceholderAmount) {
+		addPlaceholders(defaultBusModelPlaceholderAmount - busModelPlaceholderAmount);
+		busModelPlaceholderAmount = defaultBusModelPlaceholderAmount;
+	}
+	for (const p of $busModelList.children('.placeholder')) {
+		p.insertAdjacentHTML("afterbegin", `
+            <div class="skeleton"></div>
+            <div class="skeleton"></div>
+            <div class="skeleton"></div>
+        `);
+	}
+}
+
 async function getBusModels() {
-	const response = await fetch(`/api/admin/buses/models?page=${pageNum}`)
+
+	/*await new Promise(r => setTimeout(r, 5000));*/
+
+	const response = await fetch(`/api/admin/buses/models?page=${pagination.curPage - 1}`)
 	const data = await response.json()
-	lastPage = data.totalPages - 1
+	pagination.lastPage = data.totalPages
 	busModelList = data.content
 }
 
 function fillPageWithModels() {
+	$busModelList.children('.placeholder').remove();
+	busModelPlaceholderAmount = busModelList.length;
+	addPlaceholders(busModelPlaceholderAmount);
+
+	let $placeholders = $busModelList.children('.placeholder');
 	let models = busModelList
 
 	for (let i = 0; i < models.length; i++) {
 		let model = models[i]
 
 		let pageTemplate = `
-            <div class="col models-list-content py-3 m-2" id="model-${model.id}">
-                <div class="text-center model-info">
+            <div class="model-list-content" id="model-${model.id}">
+                <div class="model-info">
                     <p class="model-name">${model.name}</p>
                     <p class="model-class">${model.busClassName}-класс</p>
                     <p class="model-seats">Мест: ${model.numberOfSeats}</p>
                 </div>
-                <div class="model-delete" id="rm-${model.id}"><i class="fas fa-trash"></i></div>
+                <div class="model-func">
+                    <div class="model-edit" id="edit-${model.id}"><i class="fas fa-pen"></i></div>
+                    <div class="model-delete" id="rm-${model.id}"><i class="fas fa-trash"></i></div>
+                </div>
             </div>`
 
-		$('.model-list-elements').append(pageTemplate)
+		$placeholders[i].insertAdjacentHTML("afterbegin", pageTemplate);
 
-		$(`#rm-${model.id}`).on('click', function () {
+		$(`#rm-${model.id}`).on('click', () => {
 			if (confirm("Удалить модель автобуса?")) {
 				removeModel(model.id)
 				$(`#model-${model.id}`).remove()
 			}
 		})
 	}
-
-	$('[id^=page-link-]').remove()
-
-	if (lastPage > 0) {
-		for (let i = 0; i < lastPage + 1; i++) {
-			let num = i + 1
-			let pageTemplate = `
-            <li class="page-item" id="page-link-${num}">
-                <a class="pagination-link ${pageNum === i ? "current" : ""}" id="page-${num}" aria-label="${num}">
-                    <span aria-hidden="true">${num}</span>
-                </a>
-            </li>`
-
-			$('#page-next-li').before(pageTemplate)
-
-			if (pageNum !== i) {
-				$(`#page-link-${num}`).on('click', () => {
-					pageNum = i
-					$('.model-list-elements *').remove()
-
-					if ($('#model-filter').val() !== "") {
-						filterBuses().then(fillPageWithModels)
-					} else {
-						getBusModels().then(fillPageWithModels)
-					}
-				})
-			}
-		}
-		$('#pagination').show()
-	} else {
-		$('#pagination').hide()
-	}
-}
-
-async function filterBuses() {
-	let filter = $('#model-filter').val()
-
-	const response = await fetch(`/api/admin/buses/models?page=${pageNum}&filter=${filter}`)
-	const data = await response.json()
-	$('.model-list-elements *').remove()
-	lastPage = data.totalPages - 1
-	busModelList = data.content
-}
-
-function fillSelectWithClasses() {
-	for (let i = 0; i < busClassList.length; i++) {
-		let busClass = busClassList[i]
-
-		let pageTemplate = `<li>${busClass.name}</li>`
-		$('#select-class ul').append(pageTemplate)
-	}
-	selectParts.push({
-		$input: $('#select-class input'),
-		$arrow: $('#select-class .select-arrow'),
-		$list: $('#select-class ul'),
-		$options: $('#select-class li'),
-		valueArray: busClassList
-	})
-	addHandlersForSelect(0, 'Класс автобуса', 'Класс автобуса')
+	pagination.update();
 }
 
 async function removeModel(id) {
@@ -146,36 +154,48 @@ async function removeModel(id) {
 	});
 }
 
-$('#model-filter').on('input', () => {
+/*async function filterBuses() {
+	let filter = $('#model-filter').val()
+
+	const response = await fetch(`/api/admin/buses/models?page=${pagination.curPage - 1}&filter=${filter}`)
+	const data = await response.json()
+	$('.model-list-elements *').remove()
+	//lastPage = data.totalPages - 1
+	//busModelList = data.content
+}*/
+
+function fillUISelectWithClasses() {
+	const $select = UISelectContainer.get('bus-class');
+	for (let i = 0; i < busClassList.length; i++) {
+		const busClass = busClassList[i];
+		const templ = `<li>${busClass.name}</li>`;
+		$select.$list.append(templ);
+	}
+	$select.updateOptions();
+}
+
+function createUISelectObject() {
+	const $busClass = $('#select-class');
+	const placeholders = {
+		focused: 'Фильтр по названию',
+		blurred: 'Класс автобуса'
+	};
+	const busClassSelect = new CustomSelect($busClass, placeholders, busClassList);
+	UISelectContainer.set('bus-class', busClassSelect);
+}
+
+function addPlaceholders(amount) {
+	let templ = '';
+	for (let i = 0; i < amount; i++) {
+		templ += '<div class="placeholder"></div>';
+	}
+	$busModelList.append(templ);
+}
+
+/*$('#model-filter').on('input', () => {
 	clearTimeout()
 	pageNum = 0
 	setTimeout(function () {
 		filterBuses().then(fillPageWithModels)
 	}, 800)
-})
-
-$('#page-prev').on('click', () => {
-	if (pageNum > 0) {
-		pageNum--
-		$('.model-list-elements *').remove()
-
-		if ($('#model-filter').val() !== "") {
-			filterBuses().then(fillPageWithModels)
-		} else {
-			getBusModels().then(fillPageWithModels)
-		}
-	}
-})
-
-$('#page-next').on('click', () => {
-	if (pageNum < lastPage) {
-		pageNum++
-		$('.model-list-elements *').remove()
-
-		if ($('#model-filter').val() !== "") {
-			filterBuses().then(fillPageWithModels)
-		} else {
-			getBusModels().then(fillPageWithModels)
-		}
-	}
-})
+})*/
